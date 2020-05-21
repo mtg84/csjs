92b9d63fbf92ac765abfd969f28eca2dda117b6b
Maria <formacionjee@gmail.com>
2020-05-21 15:32:00 +0200

    Update section 17

---
+++ section17/04/app.js
@@ -0,0 +1,55 @@
+// const firstReq = new XMLHttpRequest();
+// firstReq.addEventListener('load', function() {
+// 	console.log('FIRST REQUEST WORKED!!!');
+// 	const data = JSON.parse(this.responseText);
+// 	const filmURL = data.results[0].films[0];
+// 	const filmReq = new XMLHttpRequest();
+// 	filmReq.addEventListener('load', function() {
+// 		console.log('SECOND REQUEST WORKED!!!');
+// 		const filmData = JSON.parse(this.responseText);
+// 		console.log(filmData.title);
+// 	});
+// 	filmReq.addEventListener('error', function(e) {
+// 		console.log('ERROR!!', e);
+// 	});
+// 	filmReq.open('GET', filmURL);
+// 	filmReq.send();
+// });
+// firstReq.addEventListener('error', (e) => {
+// 	console.log('ERROR!!!!!!');
+// });
+// firstReq.open('GET', 'https://swapi.co/api/planets/');
+// firstReq.send();
+// console.log('Request Sent!');
+
+fetch('https://swapi.dev/api/planets/')
+	.then((response) => {
+		if (!response.ok)
+			throw new Error(`Status Code Error: ${response.status}`);
+
+		// returning a promise so we can chain
+		return response.json();
+	})
+	.then((data) => {
+		console.log('FETCHED ALL PLANETS (first 10)');
+		console.log('data',data.results);
+		const filmURL = data.results[0].films[0];
+
+		// returning a promise so we can chain
+		return fetch(filmURL);
+	})
+	.then((response) => {
+		if (!response.ok)
+			throw new Error(`Status Code Error: ${response.status}`);
+		
+		// returning a promise so we can chain
+		return response.json();
+	})
+	.then((data) => {
+		console.log('FETCHED FIRST FILM, based off of first planet');
+		console.log(data.title);
+	})
+	.catch((err) => {
+		console.log('SOMETHING WENT WRONG WITH FETCH!');
+		console.log(err);
+	});
